973a8d1828f5b7601076f11e8d80b0c5d745483b
nimmagadda1982 <nimmagadda1982@gmail.com>
2022-07-27 15:50:49 +0530

    App changes

--- NavaRohiApp/src/app/app.module.ts
+++ NavaRohiApp/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
+import { CareersComponent } from './careers/careers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
      HeaderComponent,
+     ContactComponent,
+     CareersComponent,
   ],
   imports: [
     BrowserModule,
