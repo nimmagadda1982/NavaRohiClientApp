5356a8eccc07cb0aa04cc9a2dd57d9dc6532637e
nimmagadda1982 <nimmagadda1982@gmail.com>
2022-07-27 15:17:05 +0530

    Added compoments

--- NavaRohiApp/src/app/app.module.ts
+++ NavaRohiApp/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+     HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
